17122bdd7345cb923aff709268b9e68a7ef705d7
Clay Sullivan <clay@Clays-MacBook-Pro.local>
2017-05-11 14:46:04 -0400

    SandwichCalculator class

--- src/main/java/hello/SandwichCalculator.java
+++ src/main/java/hello/SandwichCalculator.java
@@ -1,5 +1,26 @@
 package hello;
 
 public class SandwichCalculator {
+	private final int NUM_QUESTIONS = 3;
 
+	private int[] numberData = new int[NUM_QUESTIONS];
+	
+	// method to enter the score into the array
+	public void enterScore(int questionNumber)
+	{
+		
+	}
+	
+	// method that returns the score to the GreetingController
+	public int getScore()
+	{
+		int totalScore = 0;
+		
+		for (int i = 0; i < numberData.length; i++)
+		{
+			totalScore = totalScore + numberData[i];
+		}
+		
+		return totalScore;
+	}
 }
